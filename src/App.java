bbd0cafaed139535496d4bd2b7b4d6f95e323fe0
ma-teo-12 <josegallecriollo12@gmail.com>
2026-04-20 19:58:32 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        metodoBurbuja();
+
          
+    }
+    public static void metodoBurbuja(){
+
+        System.out.println("METODO BURBUJA");
+
+        int[] arreglo = new int[] {10, -5, 0, 2, 7};
+        MetodoBurbuja metodoBurbuja = new MetodoBurbuja(arreglo);
+        metodoBurbuja.ImprimirArreglo();
+        metodoBurbuja.OrdenarAcendente();
+        metodoBurbuja.ImprimirArreglo();
+        
+        metodoBurbuja.OrdenarDescendente();
+        metodoBurbuja.ImprimirArreglo();
+
+        
     }
 }
